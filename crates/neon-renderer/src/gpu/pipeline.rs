09cd1ac654832de6c99f7fea42f5ee639e7b7fe5
Rithul Kamesh <hi@rithul.dev>
2026-08-28 21:53:08 +0530

    feat(renderer): inline shader `test.wgsl` directly into pipeline.rs

--- crates/neon-renderer/src/gpu/pipeline.rs
+++ crates/neon-renderer/src/gpu/pipeline.rs
@@ -12,8 +12,40 @@ pub struct NFPipeline {
     pub(crate) raw: RenderPipeline,
 }
 
+const SHADER: &str = r#"
+struct CameraUniform {
+    view_proj: mat4x4<f32>,
+};
+@group(0) @binding(0)
+var<uniform> camera: CameraUniform;
+
+struct VertexInput {
+    @location(0) position: vec3<f32>,
+    @location(1) color: vec3<f32>,
+}
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) color: vec3<f32>,
+}
+
+@vertex
+fn vs_main(model: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.color = model.color;
+    out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
+    return out;
+}
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(in.color, 1.0);
+}
+"#;
+
 impl NFPipeline {
-    #[instrument(name = "pipeline.new", skip(device, camera_bind_group_layout), fields(format = ?format))]
+    #[instrument(name = "pipeline.new", skip(device, camera_bind_group_layout), fields(format = ?format
+    ))]
     pub fn new(
         device: &Device,
         format: TextureFormat,
@@ -21,7 +53,7 @@ impl NFPipeline {
     ) -> Self {
         let shader = device.create_shader_module(ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: ShaderSource::Wgsl(include_str!("shaders/test.wgsl").into()),
+            source: ShaderSource::Wgsl(SHADER.into()),
         });
 
         debug!(?format, "building render pipeline");
